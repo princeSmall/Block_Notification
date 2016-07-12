0fea8a2383d0cdf5d559bf0ea5705a71d576e06d
Tongle <Tongle@sansi.com>
2016-07-12 10:17:32 +0800

    Initial Commit

---
+++ block-mark/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  block-mark
+//
+//  Created by tongle on 16/7/12.
+//  Copyright © 2016年 tongle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ block-markTests/block_markTests.m
@@ -0,0 +1,39 @@
+//
+//  block_markTests.m
+//  block-markTests
+//
+//  Created by tongle on 16/7/12.
+//  Copyright © 2016年 tongle. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface block_markTests : XCTestCase
+
+@end
+
+@implementation block_markTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
